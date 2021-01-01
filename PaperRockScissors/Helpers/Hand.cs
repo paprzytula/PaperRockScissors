b0549a220b1928bc6ce8874d7889f20449df2948
Paweł <paprzytula@gmail.com>
2021-01-01 21:44:26 +0100

    Add project files.

---
+++ PaperRockScissors/Helpers/Hand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaperRockScissors.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinsAgainst { get; set; }
+        public OptionRPS LosesAgainst { get; set; }
+        public string Image { get; set; }
+
+        public GameStatus PlayAgainst(Hand oponentHand)
+        {
+
+            if (Selection == oponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            if (LosesAgainst == oponentHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+                return GameStatus.Victory;
+        }
+
+
+
+    }
+}
---
+++ PaperRockScissors/Pages/Index.razor.cs
@@ -0,0 +1,78 @@
+﻿using PaperRockScissors.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PaperRockScissors.Pages
+{
+    public partial class Index : IDisposable
+    {
+        List<Hand> hands = new List<Hand>()
+        {
+            new Hand{Selection = OptionRPS.Rock, LosesAgainst = OptionRPS.Paper, WinsAgainst = OptionRPS.Scissors, Image="images/rock.jpg"},
+            new Hand{Selection = OptionRPS.Paper, WinsAgainst = OptionRPS.Rock, LosesAgainst= OptionRPS.Scissors, Image="images/paper.jpg"},
+            new Hand{Selection = OptionRPS.Scissors, LosesAgainst = OptionRPS.Rock, WinsAgainst= OptionRPS.Paper, Image="images/scissors.jpg"}
+        };
+
+
+        public Timer Timer { get; set; }
+        Hand oponentHand;
+        string resultMessage = string.Empty;
+        string resultMessageColor = string.Empty;
+        public void Dispose()
+        {
+            if (Timer != null)
+            {
+                Timer.Dispose();
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            oponentHand = hands[0];
+            Timer = new Timer();
+            Timer.Interval = 100;
+            Timer.Elapsed += TimerOnElapsed;
+            Timer.Start();
+        }
+        int indexOponentHand = 0;
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            indexOponentHand = (indexOponentHand + 1) % hands.Count;
+            oponentHand = hands[indexOponentHand];
+            StateHasChanged();
+        }
+        private void SelectHand(Hand hand)
+        {
+            Timer.Stop();
+            var result = hand.PlayAgainst(oponentHand);
+
+            if (result == GameStatus.Victory)
+            {
+                resultMessageColor = "green";
+                resultMessage = "You WIN!";
+            }
+            else if (result == GameStatus.Loss)
+            {
+                resultMessageColor = "red";
+                resultMessage = "You Loose!";
+            }
+            else
+            {
+                resultMessageColor = "bue";
+                resultMessage = "It's a DRAW. Try Again?";
+            }
+
+        }
+
+        private void PlayAgain()
+        {
+
+            Timer.Start();
+            resultMessage = string.Empty;
+        }
+    }
+
+}
